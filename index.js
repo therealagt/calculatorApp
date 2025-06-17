3e4bef017722881e81f324320dbb1c4bb48661a7
therealagt <210968687+therealagt@users.noreply.github.com>
2025-06-17 20:50:48 +0200

    operating functions and basic html design

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ const divide = (a, b) => {
     }
     return a / b; 
 };
+
+// operating & display //
+
+const operate = function(a, b, operator) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+        default:
+            return "Invalid operator";
+    }
+    display.textContent = result; 
+    return result; 
+}
